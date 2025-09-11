3e688b6780aaf50da6d634547009c35273548b4d
Sergei <lordistor@gmail.com>
2025-09-11 20:09:44 +0300

    add update

--- scripts/authorization.js
+++ scripts/authorization.js
@@ -1,114 +1,127 @@
-const registrationForm = document.querySelector('#registration-form');
+document.addEventListener('DOMContentLoaded', () => {
+    const registrationForm = document.querySelector('#registration-form');
+    const authorizationForm = document.querySelector('#authorization-form');
+    const registrationNameError = document.querySelector('#registrationNameError');
+    const registrationPasswordError = document.querySelector('#registrationPasswordError');
+    const authorizationNameError = document.querySelector('#authorizationNameError');
+    const authorizationPasswordError = document.querySelector('#authorizationPasswordError');
 
-registrationForm.addEventListener('submit', (event) => {
-    event.preventDefault();
 
-    document.querySelector('#registrationNameError').textContent = '';
-    document.querySelector('#registrationPasswordError').textContent = '';
-    let hasError = false;
-    
-    const users = JSON.parse(localStorage.getItem('users')) || [];
+    registrationForm.addEventListener('submit', (event) => {
+        event.preventDefault();
 
-    const user = Array.from(event.target.elements)
-        .reduce((acc, element) => {
-            const { name, value, type, checked } = element;
-            if (type === 'submit') {
+        registrationNameError.textContent = '';
+        registrationPasswordError.textContent = '';
+        let hasError = false;
+        
+        const users = JSON.parse(localStorage.getItem('users')) || [];
+
+        const user = Array.from(event.target.elements)
+            .reduce((acc, element) => {
+                const { name, value, type, checked } = element;
+                if (type === 'submit') {
+                    return acc;
+                }
+                acc[name] = value;
+                if (name === 'checkbox') {
+                    acc[name] = checked;
+                }
                 return acc;
-            }
-            acc[name] = value;
-            if (name === 'checkbox') {
-                acc[name] = checked;
-            }
-            return acc;
-        }, {});
+            }, {});
 
-    if (user.userName.length < 4) {
-        document.querySelector('#registrationNameError').textContent = 'Логин должен содержать не менее 4-х символов';
-        hasError = true;
-    }
-
-    if (user.userPassword.length < 4) {
-        document.querySelector('#registrationPasswordError').textContent = 'Пароль должен содержать не менее 4-х символов';
-        hasError = true;
-    }
-
-    if (user.userName === '') {
-        document.querySelector('#registrationNameError').textContent = 'Поле не должно быть пустым';
-        hasError = true;
-    }
-
-    if (user.userPassword === '') {
-        document.querySelector('#registrationPasswordError').textContent = 'Поле не должно быть пустым';
-        hasError = true;
-    }
-    
-    if(!hasError) {
-        const userFromBD = users.find((item) => item.userName === user.userName);
-        if (!userFromBD) {
-            users.push(user);
-            localStorage.setItem('users', JSON.stringify(users));
-            localStorage.setItem('isLoginSuccess', JSON.stringify(true));
-            window.location.href = '/';
-        } else {
-            document.querySelector('#registrationNameError').textContent = 'Пользователь уже зарегистрирован';
+        if (user.userName.length < 4) {
+            registrationNameError.textContent = 'Логин должен содержать не менее 4-х символов';
             hasError = true;
         }
-    }
-})
 
-const authorizationForm = document.querySelector('#authorization-form');
-
-authorizationForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    
-    document.querySelector('#authorizationNameError').textContent = '';
-    document.querySelector('#authorizationPasswordError').textContent = '';
-    let hasError = false;
-    
-    const users = JSON.parse(localStorage.getItem('users')) || [];
-
-    const user = Array.from(event.target.elements)
-        .reduce((acc, element) => {
-            const { name, value, type } = element;
-            if (type === 'submit') {
-                return acc;
-            }
-            acc[name] = value;
-            return acc;
-        }, {});
-
-    if (user.userName === '') {
-        document.querySelector('#authorizationNameError').textContent = 'Поле не должно быть пустым';
-        hasError = true;
-    }
-
-    if (user.userPassword === '') {
-        document.querySelector('#authorizationPasswordError').textContent = 'Поле не должно быть пустым';
-        hasError = true;
-    }
-    
-    if (!hasError) {
-        const userFromBD = users.find((item) => item.userName === user.userName);
-        if (userFromBD && user.userPassword === userFromBD?.userPassword) {
-            localStorage.setItem('isLoginSuccess', JSON.stringify(true));
-            window.location.href = '/';
+        if (user.userPassword.length < 4) {
+            registrationPasswordError.textContent = 'Пароль должен содержать не менее 4-х символов';
+            hasError = true;
         }
+
+        if (user.userName === '') {
+            registrationNameError.textContent = 'Поле не должно быть пустым';
+            hasError = true;
+        }
+
+        if (user.userPassword === '') {
+            registrationPasswordError.textContent = 'Поле не должно быть пустым';
+            hasError = true;
+        }
+        
+        if(!hasError) {
+            const userFromBD = users.find((item) => item.userName === user.userName);
+            if (!userFromBD) {
+                user.basket = [];
+                users.push(user);
+                localStorage.setItem('users', JSON.stringify(users));
+                toggleToAuth();
+            } else {
+                registrationNameError.textContent = 'Пользователь уже зарегистрирован';
+                hasError = true;
+            }
+        }
+    })
+
+
+    authorizationForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        
+        authorizationNameError.textContent = '';
+        authorizationPasswordError.textContent = '';
+        let hasError = false;
+        
+        const users = JSON.parse(localStorage.getItem('users')) || [];
+
+        const user = Array.from(event.target.elements)
+            .reduce((acc, element) => {
+                const { name, value, type } = element;
+                if (type === 'submit') {
+                    return acc;
+                }
+                acc[name] = value;
+                return acc;
+            }, {});
+
+        if (user.userName === '') {
+            authorizationNameError.textContent = 'Поле не должно быть пустым';
+            hasError = true;
+        }
+
+        if (user.userPassword === '') {
+            authorizationPasswordError.textContent = 'Поле не должно быть пустым';
+            hasError = true;
+        }
+        
+        if (!hasError) {
+            const userFromBD = users.find((item) => item.userName === user.userName);
+            if (userFromBD && user.userPassword === userFromBD?.userPassword) {
+                localStorage.setItem('authorizedUser', JSON.stringify(userFromBD));
+                window.location.href = '/';
+            } else {
+                authorizationNameError.textContent = 'Логин или Пароль не верный';
+                authorizationPasswordError.textContent = 'Логин или Пароль не верный';
+            }
+        }
+    })
+
+    const toggleToAuth = () => {
+        const authorization = document.querySelector('.authorization');
+        const registration = document.querySelector('.registration');
+        authorization.style.display = 'block';
+        registration.style.display = 'none';
     }
+
+    document.querySelector('.authorization-link a').addEventListener('click', event => {
+        event.preventDefault();
+        const authorization = document.querySelector('.authorization');
+        const registration = document.querySelector('.registration');
+        authorization.style.display = 'none';
+        registration.style.display = 'block';
+    }) 
+
+    document.querySelector('.registration-link a').addEventListener('click', event => {
+        event.preventDefault();
+        toggleToAuth();
+    })
 })
-
-document.querySelector('.authorization-link a').addEventListener('click', event => {
-    event.preventDefault();
-    const authorizationForm = document.querySelector('.authorization');
-    const registrationForm = document.querySelector('.registration');
-    authorizationForm.style.display = 'none';
-    registrationForm.style.display = 'block';
-}) 
-
-document.querySelector('.registration-link a').addEventListener('click', event => {
-    event.preventDefault();
-    const authorizationForm = document.querySelector('.authorization');
-    const registrationForm = document.querySelector('.registration');
-    authorizationForm.style.display = 'block';
-    registrationForm.style.display = 'none';
-})
-
--- scripts/index.js
+++ scripts/index.js
@@ -1,212 +1,215 @@
-let products = [
-  {
-    id: 0,
-    preview: './images/1.svg',
-    title: 'Устрицы по рокфеллеровски',
-    description: 'Значимость этих проблем настолько очевидна, что укрепление и развитие структуры',
-    price: 2700
-  },
-  {
-    id: 1,
-    preview: './images/2.svg',
-    title: 'Свиные ребрышки на гриле с зеленью',
-    description: 'Не следует, однако забывать, что реализация намеченных плановых',
-    price: 1600
-  },
-  {
-    id: 2,
-    preview: './images/3.svg',
-    title: 'Креветки по-королевски в лимонном соке',
-    description: 'Значимость этих проблем настолько очевидна, что укрепление и развитие структуры обеспечивает широкому кругу',
-    price: 1820
-  },
-  {
-    id: 3,
-    preview: './images/1.svg',
-    title: 'Устрицы по рокфеллеровски',
-    description: 'Значимость этих проблем настолько очевидна, что укрепление и развитие структуры ',
-    price: 2700
-  },
-  {
-    id: 4,
-    preview: './images/1.svg',
-    title: 'Устрицы по рокфеллеровски',
-    description: 'Значимость этих проблем настолько очевидна, что укрепление и развитие структуры',
-    price: 2700
-  },
-  {
-    id: 5,
-    preview: './images/2.svg',
-    title: 'Свиные ребрышки на гриле с зеленью',
-    description: 'Не следует, однако забывать, что реализация намеченных плановых',
-    price: 1600
-  },
-  {
-    id: 6,
-    preview: './images/3.svg',
-    title: 'Креветки по-королевски в лимонном соке',
-    description: 'Значимость этих проблем настолько очевидна, что укрепление и развитие структуры обеспечивает широкому кругу',
-    price: 1820
-  },
-  {
-    id: 7,
-    preview: './images/1.svg',
-    title: 'Устрицы по рокфеллеровски',
-    description: 'Значимость этих проблем настолько очевидна, что укрепление и развитие структуры ',
-    price: 2700
+document.addEventListener('DOMContentLoaded', () => {
+
+    let products = [
+    {
+      id: 0,
+      preview: './images/1.svg',
+      title: 'Устрицы по рокфеллеровски',
+      description: 'Значимость этих проблем настолько очевидна, что укрепление и развитие структуры',
+      price: 2700
+    },
+    {
+      id: 1,
+      preview: './images/2.svg',
+      title: 'Свиные ребрышки на гриле с зеленью',
+      description: 'Не следует, однако забывать, что реализация намеченных плановых',
+      price: 1600
+    },
+    {
+      id: 2,
+      preview: './images/3.svg',
+      title: 'Креветки по-королевски в лимонном соке',
+      description: 'Значимость этих проблем настолько очевидна, что укрепление и развитие структуры обеспечивает широкому кругу',
+      price: 1820
+    },
+    {
+      id: 3,
+      preview: './images/1.svg',
+      title: 'Устрицы по рокфеллеровски',
+      description: 'Значимость этих проблем настолько очевидна, что укрепление и развитие структуры ',
+      price: 2700
+    },
+    {
+      id: 4,
+      preview: './images/1.svg',
+      title: 'Устрицы по рокфеллеровски',
+      description: 'Значимость этих проблем настолько очевидна, что укрепление и развитие структуры',
+      price: 2700
+    },
+    {
+      id: 5,
+      preview: './images/2.svg',
+      title: 'Свиные ребрышки на гриле с зеленью',
+      description: 'Не следует, однако забывать, что реализация намеченных плановых',
+      price: 1600
+    },
+    {
+      id: 6,
+      preview: './images/3.svg',
+      title: 'Креветки по-королевски в лимонном соке',
+      description: 'Значимость этих проблем настолько очевидна, что укрепление и развитие структуры обеспечивает широкому кругу',
+      price: 1820
+    },
+    {
+      id: 7,
+      preview: './images/1.svg',
+      title: 'Устрицы по рокфеллеровски',
+      description: 'Значимость этих проблем настолько очевидна, что укрепление и развитие структуры ',
+      price: 2700
+    }
+  ]
+  const authorizedUser = JSON.parse(localStorage.getItem('authorizedUser'));
+
+  if (!authorizedUser) {
+    window.location.href = 'login.html';
   }
-]
 
-let basketList = JSON.parse(localStorage.getItem('basket')) ?? [];
-const isLogin = JSON.parse(localStorage.getItem('isLoginSuccess'));
-console.log(typeof isLogin);
-if (!isLogin) {
-  window.location.href = 'login.html';
-}
-
-const renderCounter = () => {
-  const headerInner = document.querySelector('.header__inner-info');
-  const basketCount = document.querySelector('.basket__count');
-  const counter = basketList.reduce((acc, item) => {
-    return {count: acc.count + item.count, price: acc.price + item.price * item.count};
-  }, {count: 0, price: 0});
-  headerInner.innerHTML = `
-    ${counter.count} товара <br>
-    на сумму ${counter.price} ₽
-  `
-  basketCount.innerHTML = counter.count;
-}
-renderCounter();
-const updateBasketCounter = (operation) => {
-  const basketCount = document.querySelector('.basket__count');
-  basketCount.innerHTML = operation === '+' ? Number(basketCount.innerHTML) + 1 : Number(basketCount.innerHTML) - 1;
+  const renderCounter = () => {
+    const headerInner = document.querySelector('.header__inner-info');
+    const basketCount = document.querySelector('.basket__count');
+    const counter = authorizedUser.basket.reduce((acc, item) => {
+      return {count: acc.count + item.count, price: acc.price + item.price * item.count};
+    }, {count: 0, price: 0});
+    headerInner.innerHTML = `
+      ${counter.count} товара <br>
+      на сумму ${counter.price} ₽
+    `
+    basketCount.innerHTML = counter.count;
+  }
   renderCounter();
-}
+  const updateBasketCounter = (operation) => {
+    const basketCount = document.querySelector('.basket__count');
+    basketCount.innerHTML = operation === '+' ? Number(basketCount.innerHTML) + 1 : Number(basketCount.innerHTML) - 1;
+    renderCounter();
+  }
 
-const renderProducts = () => {
-  const productsList = document.querySelector('.products-list');
-  products.forEach((item) => {
-    const newDiv = document.createElement('div');
-    newDiv.className = 'products-card';
-    newDiv.innerHTML = `
-      <img src="${item.preview}" alt="" class="products-card__preview">
-      <h2 class="products-card__title">
-        ${item.title}
-      </h2>
-      <p class="products-card__description">
-        ${item.description} 
-      </p>
-      <div class="products-card__bottom">
-        <span class="products-card__bottom-price">
-          ${item.price} ₽
-        </span>
-        <button class="products-card__bottom-btn" id="add-basket"></button>
-      </div>
-    `;
-    newDiv.addEventListener('click', ({target}) => {
-      const className = target.className;
-      if (className === 'products-card__bottom-btn') {
-        const findedItem = basketList.find((basketItem) => item.id === basketItem.id);
-        if(!findedItem) {
-          basketList.push({...item, count: 1});
-        } else {
-          findedItem.count += 1;
+  const renderProducts = () => {
+    const productsList = document.querySelector('.products-list');
+    products.forEach((item) => {
+      const newDiv = document.createElement('div');
+      newDiv.className = 'products-card';
+      newDiv.innerHTML = `
+        <img src="${item.preview}" alt="" class="products-card__preview">
+        <h2 class="products-card__title">
+          ${item.title}
+        </h2>
+        <p class="products-card__description">
+          ${item.description} 
+        </p>
+        <div class="products-card__bottom">
+          <span class="products-card__bottom-price">
+            ${item.price} ₽
+          </span>
+          <button class="products-card__bottom-btn" id="add-basket"></button>
+        </div>
+      `;
+      newDiv.addEventListener('click', ({target}) => {
+        const className = target.className;
+        if (className === 'products-card__bottom-btn') {
+          const findedItem = authorizedUser.basket.find((basketItem) => item.id === basketItem.id);
+          if(!findedItem) {
+            authorizedUser.basket.push({...item, count: 1});
+          } else {
+            findedItem.count += 1;
+          }
+          localStorage.setItem('authorizedUser', JSON.stringify(authorizedUser));
+          updateBasketCounter('+');
         }
-        localStorage.setItem('basket', JSON.stringify(basketList));
-        updateBasketCounter('+');
-      }
-    })
-    productsList.append(newDiv);
-    
-  });
-}
-renderProducts();
-const removeElement = (el) => {
-  el.remove();
-}
-const renderBasketList = () => {
-  const basket = document.querySelector('.modal__inner-list');
+      })
+      productsList.append(newDiv);
+      
+    });
+  }
+  renderProducts();
+  const removeElement = (el) => {
+    el.remove();
+  }
+  const renderBasketList = () => {
+    const basket = document.querySelector('.modal__inner-list');
 
-  const list = basketList.map((item) => {
-    const newDiv = document.createElement('div');
-    newDiv.className = 'products-card';
-    newDiv.innerHTML = `
-      <img src="${item.preview}" alt="" class="products-card__preview">
-      <h2 class="products-card__title">
-        ${item.title}
-      </h2>
-      <p class="products-card__description">
-        ${item.description} 
-      </p>
-      <div class="products-card__count card__count">
-        <span class="card__count-minus" id="plus">-</span>
-        <span class="card__count-result" id="count-product">${item.count}</span>
-        <span class="card__count-plus" id="minus">+</span>
-      </div>
-      <div class="products-card__bottom">
-        <span class="products-card__bottom-price">
-          ${item.price * item.count} ₽
-        </span>
-        <button class="products-card__bottom-btn" id="remove-basket"></button>
-      </div>
-    `;
+    const list = authorizedUser.basket.map((item) => {
+      const newDiv = document.createElement('div');
+      newDiv.className = 'products-card';
+      newDiv.innerHTML = `
+        <img src="${item.preview}" alt="" class="products-card__preview">
+        <h2 class="products-card__title">
+          ${item.title}
+        </h2>
+        <p class="products-card__description">
+          ${item.description} 
+        </p>
+        <div class="products-card__count card__count">
+          <span class="card__count-minus" id="plus">-</span>
+          <span class="card__count-result" id="count-product">${item.count}</span>
+          <span class="card__count-plus" id="minus">+</span>
+        </div>
+        <div class="products-card__bottom">
+          <span class="products-card__bottom-price">
+            ${item.price * item.count} ₽
+          </span>
+          <button class="products-card__bottom-btn" id="remove-basket"></button>
+        </div>
+      `;
 
-    newDiv.addEventListener('click', ({target}) => {
-      const className = target.className;
-      if (className === 'card__count-plus') {
-        const findedItem = basketList.find((basketItem) => item.id === basketItem.id);
-        
-        findedItem.count += 1;
-        target.parentNode.children[1].innerHTML = findedItem.count;
-        target.parentNode.parentNode.children[4].children[0].innerHTML = item.price * findedItem.count + ' ₽';
-        updateBasketCounter('+');
-      } else if (className === 'card__count-minus') {
-        const findedItem = basketList.find((basketItem) => item.id === basketItem.id);
-        
-        findedItem.count -= 1;
-        updateBasketCounter('-');
-        if (findedItem.count) {
+      newDiv.addEventListener('click', ({target}) => {
+        const className = target.className;
+        if (className === 'card__count-plus') {
+          const findedItem = authorizedUser.basket.find((basketItem) => item.id === basketItem.id);
+          
+          findedItem.count += 1;
           target.parentNode.children[1].innerHTML = findedItem.count;
           target.parentNode.parentNode.children[4].children[0].innerHTML = item.price * findedItem.count + ' ₽';
+          updateBasketCounter('+');
+        } else if (className === 'card__count-minus') {
+          const findedItem = authorizedUser.basket.find((basketItem) => item.id === basketItem.id);
           
-        } else {
+          findedItem.count -= 1;
+          updateBasketCounter('-');
+          if (findedItem.count) {
+            target.parentNode.children[1].innerHTML = findedItem.count;
+            target.parentNode.parentNode.children[4].children[0].innerHTML = item.price * findedItem.count + ' ₽';
+            
+          } else {
+            removeElement(target.parentNode.parentNode);
+            authorizedUser.basket = authorizedUser.basket.filter((basketItem) => item.id !== basketItem.id)
+          }
+          
+        } else if (className === 'products-card__bottom-btn') {
           removeElement(target.parentNode.parentNode);
-          basketList = basketList.filter((basketItem) => item.id !== basketItem.id)
+          const filteredList = authorizedUser.basket.filter((el) => item.id !== el.id);
+          authorizedUser.basket = filteredList;
+          renderBasketList();
+          renderCounter();
         }
+        localStorage.setItem('authorizedUser', JSON.stringify(authorizedUser));
         
-      } else if (className === 'products-card__bottom-btn') {
-        removeElement(target.parentNode.parentNode);
-        const filteredList = basketList.filter((el) => item.id !== el.id);
-        basketList = filteredList;
-        renderBasketList();
-        renderCounter();
-      }
-      localStorage.setItem('basket', JSON.stringify(basketList));
-      console.log(basketList);
-      
-    })
-    return newDiv;
-  });
-  basket.replaceChildren(...list);
-}
+      })
+      return newDiv;
+    });
+    basket.replaceChildren(...list);
+  }
 
-const modalButton = document.querySelector('.header__inner-basket');
-modalButton.addEventListener('click', () => {
-  const modal = document.querySelector('.modal');
-  renderBasketList();
-  modal.classList.add('visible');
+  const modalButton = document.querySelector('.header__inner-basket');
+  modalButton.addEventListener('click', () => {
+    const modal = document.querySelector('.modal');
+    renderBasketList();
+    modal.classList.add('visible');
+  })
+
+  const modalCloseButton = document.querySelector('.modal__inner-close');
+  modalCloseButton.addEventListener('click', () => {
+    const modal = document.querySelector('.modal');
+    modal.classList.remove('visible');
+  })
+
+  document.querySelector('#signout').addEventListener('click', event => {
+    localStorage.removeItem('authorizedUser');
+    const updatedUsers = JSON.parse(localStorage.getItem('users'))
+      .map((user) => user.userName === authorizedUser.userName ? authorizedUser : user);
+    localStorage.setItem('users', JSON.stringify(updatedUsers));
+    window.location.href = 'login.html';
+  })
+
 })
-
-const modalCloseButton = document.querySelector('.modal__inner-close');
-modalCloseButton.addEventListener('click', () => {
-  const modal = document.querySelector('.modal');
-  modal.classList.remove('visible');
-})
-
-document.querySelector('#signout').addEventListener('click', event => {
-  localStorage.setItem('isLoginSuccess', false);
-  window.location.href = 'login.html'
-})
-
 
 
